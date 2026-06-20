7303509f7760c448566f28ade7cb1a607c3790d1
Aztro <goiababunita@gmail.com>
2026-06-20 14:11:41 -0300

    Add files via upload

---
+++ DailyScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Edit the name and websites inside the .sh file"
+echo "Good morning [your name]! I'm opening your websites, gimme a sec!"
+brave https://github.com &
+brave youtube.com &
+echo "Done! Websites open! Have a good day!"
+
+
